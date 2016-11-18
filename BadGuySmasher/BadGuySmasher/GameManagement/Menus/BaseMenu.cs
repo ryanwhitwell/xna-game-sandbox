523f0778b67d09c4e5dc170cfb94221719693389
Ryan Whitwell <ryanwhitwell.developer@gmail.com>
2016-11-18 12:03:43 -0600

    Added BaseMenu.

---
+++ BadGuySmasher/BadGuySmasher/GameManagement/Menus/BaseMenu.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using BadGuySmasher.GameManagement.Interfaces;
+using BadGuySmasher.GameManagement.Menus;
+using BadGuySmasher.GameManagement.Menus.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace BadGuySmasher.GameManagement.Menus
+{
+  public abstract class BaseMenu
+  {
+    private SpriteFont            _menuFont;
+    private KeyboardState         _lastKeyboardState;
+    private ContentManager        _contentManager;
+    private GraphicsDeviceManager _graphicsDeviceManager;
+    private MenuState             _menuState;
+    private SpriteBatch           _spriteBatch;
+    private IGameStateManager     _gameStateManager;
+
+    public BaseMenu(ContentManager contentManager, GraphicsDeviceManager graphicsDeviceManager, string spriteFontAssetName, IGameStateManager gameStateManager)
+    {
+      if (contentManager == null)
+      {
+        throw new ArgumentNullException("contentManager");
+      }
+
+      if (graphicsDeviceManager == null)
+      {
+        throw new ArgumentNullException("graphicsDeviceManager");
+      }
+
+      if (string.IsNullOrWhiteSpace(spriteFontAssetName))
+      {
+        throw new ArgumentNullException("spriteFontAssetName");
+      }
+
+      if (gameStateManager == null)
+      {
+        throw new ArgumentNullException("gameStateManager");
+      }
+      
+      _contentManager         = contentManager;
+      _graphicsDeviceManager  = graphicsDeviceManager;
+      _lastKeyboardState      = Keyboard.GetState();
+      _spriteBatch            = new SpriteBatch(graphicsDeviceManager.GraphicsDevice);
+      _menuFont               = _contentManager.Load<SpriteFont>(spriteFontAssetName);
+      _gameStateManager       = gameStateManager;
+    }
+
+    protected SpriteBatch SpriteBatch
+    {
+      get
+      {
+        return _spriteBatch;
+      }
+    }
+
+    protected MenuState MenuState
+    {
+      get
+      {
+        return _menuState;
+      }
+
+      set
+      {
+        _menuState = value;
+      }
+    }
+
+    protected KeyboardState LastKeyboardState
+    {
+      get
+      {
+        return _lastKeyboardState;
+      }
+
+      set
+      {
+        _lastKeyboardState = value;
+      }
+    }
+
+    protected SpriteFont MenuFont
+    {
+      get
+      {
+        return _menuFont;
+      }
+    }
+
+    protected GraphicsDeviceManager GraphicsDeviceManager
+    {
+      get
+      {
+        return _graphicsDeviceManager;
+      }
+    }
+
+    protected IGameStateManager GameStateManager
+    {
+      get
+      {
+        return _gameStateManager;
+      }
+    }
+  }
+}
---
+++ BadGuySmasher/BadGuySmasher/GameManagement/Menus/StartMenu.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using BadGuySmasher.GameManagement.Interfaces;
+using BadGuySmasher.GameManagement.Menus;
+using BadGuySmasher.GameManagement.Menus.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace BadGuySmasher
+{
+  public class StartMenu : BaseMenu, IStartMenu
+  {
+    private int _numberOfPlayers;
+
+    public int NumberOfPlayers { get { return _numberOfPlayers; } }
+
+    public StartMenu(ContentManager contentManager, GraphicsDeviceManager graphics, string spriteFontAssetName, IGameStateManager gameStateManager) : base(contentManager, graphics, spriteFontAssetName, gameStateManager) { }
+
+    public MenuState UpdateInput()
+    {
+      if (base.MenuState == MenuState.Exit)
+      {
+        return MenuState.Exit;
+      }
+
+      KeyboardState newState = Keyboard.GetState();
+
+      if (newState.IsKeyUp(Keys.D1))
+      {
+        // If not down last update, key has just been pressed.
+        if (base.LastKeyboardState.IsKeyDown(Keys.D1))
+        {
+          _numberOfPlayers = 1;
+          base.MenuState = MenuState.Exit;
+        }
+      }
+      
+      if (newState.IsKeyUp(Keys.D2))
+      {
+        // If not down last update, key has just been pressed.
+        if (base.LastKeyboardState.IsKeyDown(Keys.D2))
+        {
+          // TODO: 2-Players - We currently only support 1 player, so we card-code the main menu to force the selection of 1 player
+          _numberOfPlayers = 1; 
+          base.MenuState = MenuState.Exit;
+        }
+      }
+
+      // Update saved state.
+      base.LastKeyboardState = newState;
+
+      // Update the game state with the number of players selected on this Menu
+      base.GameStateManager.WorldMapManager.SetNumberOfPlayers(_numberOfPlayers);
+
+      return base.MenuState;
+    }
+
+    public void Draw()
+    {
+      base.SpriteBatch.Begin();
+
+      string text = "Bad Guy Smasher";
+      base.SpriteBatch.DrawString(base.MenuFont, text, new Vector2(base.GraphicsDeviceManager.GraphicsDevice.Viewport.Width / 2 - base.MenuFont.MeasureString(text).Length() / 2, base.GraphicsDeviceManager.GraphicsDevice.Viewport.Height / 2), Color.Black);
+
+      text = "Please select a number of Players to continue (1 or 2)";
+      base.SpriteBatch.DrawString(base.MenuFont, text, new Vector2(base.GraphicsDeviceManager.GraphicsDevice.Viewport.Width / 2 - base.MenuFont.MeasureString(text).Length() / 2, base.GraphicsDeviceManager.GraphicsDevice.Viewport.Height / 2 + base.MenuFont.LineSpacing), Color.Black);
+
+      base.SpriteBatch.End();
+    }
+  }
+}
